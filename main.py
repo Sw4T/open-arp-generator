ae6f73bf36e4039e4b6fb0a74bc2c5c8685d2770
Sw4T <pierre.gambier@yahoo.fr>
2017-11-24 14:22:04 +0100

    Reworked example to play each second two notes

--- main.py
+++ main.py
@@ -4,31 +4,36 @@ import constants
 
 # Instantiate a MIDI Pattern (contains a list of tracks)
 pattern = midi.Pattern()
-pattern.resolution = 2000
+pattern.resolution = 1000
 
 # Instantiate a MIDI Track (contains a list of MIDI events)
 track = midi.Track()
 # Append the track to the pattern
 pattern.append(track)
+
 # Instantiate a MIDI note on event, append it to the track
 
 TICK_IN_SECONDS = 0.0005
 NBTICK_FOR_A_SECOND = int(1 / TICK_IN_SECONDS)
 
-start_tick = 0
-
+# Play a G_3 and a G_2 every second
 for i in range(0, 20):
 
+	start_tick = 0
 	on = midi.NoteOnEvent(tick=start_tick, velocity=30, pitch=midi.G_3)
+	track.append(on)
+	on = midi.NoteOnEvent(tick=start_tick, velocity=60, pitch=midi.G_2)
 	track.append(on)
 	
 	# Instantiate a MIDI note off event, append it to the track
-	start_tick = start_tick + (NBTICK_FOR_A_SECOND / 2)
-	off = midi.NoteOffEvent(tick=start_tick, pitch=midi.G_3)
+	start_tick = NBTICK_FOR_A_SECOND
+	off = midi.NoteOffEvent(tick=start_tick, pitch=midi.G_2)
+	track.append(off)
+	off = midi.NoteOffEvent(tick=start_tick, pitch=midi.G_3)	
 	track.append(off)
 
 # Add the end of track event, append it to the track
-eot = midi.EndOfTrackEvent(tick=int(start_tick + 1))
+eot = midi.EndOfTrackEvent(tick=start_tick + 1)
 track.append(eot)
 # Print out the pattern
 print pattern
