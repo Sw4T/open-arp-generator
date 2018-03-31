df68987799e2556ae1274e9fda7dc23310c39280
Sw4T <pierre.gambier@yahoo.fr>
2018-03-31 03:13:48 +0200

    Added a basic design for base parameters (BPM, Pitch, Fundamental note), refactoring

---
+++ arp_properties.py
@@ -0,0 +1,6 @@
+class ArpProperties:
+
+    def __init__(self, bpm, base_note, pitch_value):
+        self.bpm = bpm
+        self.base_note = base_note
+        self.pitch_value = pitch_value
--- constants.py
+++ constants.py
@@ -1,12 +1,18 @@
 ##
-## Constants
+## Constants for MIDI
 ##
 
 OCTAVE_MAX_VALUE = 12
 OCTAVE_VALUES = range( OCTAVE_MAX_VALUE )
 
-NOTE_NAMES = ['C','Cs','D','Ds','E','F','Fs','G','Gs','A','As','B']
+NOTE_NAMES = ['C', 'C#', 'D', 'D#', 'E','F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 WHITE_KEYS = [0, 2, 4, 5, 7, 9, 11]
 BLACK_KEYS = [1, 3, 6, 8, 10]
-NOTE_PER_OCTAVE = len( NOTE_NAMES )
-NOTE_VALUES = range( OCTAVE_MAX_VALUE * NOTE_PER_OCTAVE )
+NOTE_PER_OCTAVE = len(NOTE_NAMES)
+NOTE_VALUES = range(OCTAVE_MAX_VALUE * NOTE_PER_OCTAVE)
+
+##
+## Application globals
+##
+
+VERSION_NUMBER = "0.1"
