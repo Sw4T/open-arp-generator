323fc4a43b3df16fe70c534ea2182ba76ab50ddd
LIBERMANN Nathan <nathan.libermann@inria.fr>
2018-03-31 18:06:22 +0200

    gen_seq

---
+++ sequence_generator.py
@@ -0,0 +1,36 @@
+import matrix
+import random
+
+
+def matrix_random_walk(matrix, length_sequence, init_x, init_y):
+
+	position_x = init_x
+	position_y = init_y
+	cur_symbol = matrix[position_y][position_x]
+
+	list_symbol = []
+	list_symbol.append(cur_symbol)
+
+	for i in range(length_sequence):
+		x_or_y = bool(random.getrandbits(1)) # True mean x, False mean y
+		right_or_left = bool(random.getrandbits(1)) # True mean Right(Down), False mean Left(Up)
+
+		if x_or_y: # On bouge sur l'axe des x
+			if right_or_left: # On bouge vers la droite
+				position_x = (position_x + cur_symbol+1) % 7
+				cur_symbol = matrix[position_y][position_x]
+			else: # On bouge vers la gauche
+				position_x = (position_x - cur_symbol+1) % 7
+				cur_symbol = matrix[position_y][position_x]
+		else: # On bouge sur l'axe des y
+			if right_or_left: # On bouge vers le bas
+				position_y = (position_y + cur_symbol+1) % 7
+				cur_symbol = matrix[position_y][position_x]
+			else: # On bouge vers le haut
+				position_y = (position_y - cur_symbol+1) % 7
+				cur_symbol = matrix[position_y][position_x]
+		list_symbol.append(cur_symbol)
+	return list_symbol
+
+seq = matrix_random_walk(matrix.ALPHA_ONE_7_7, 32, 3,3)
+print(seq)
